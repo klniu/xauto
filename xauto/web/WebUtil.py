5f883999356ec08360c24898a6f1afa7779de3c4
GaoXiang <hughgao01@gmail.com>
2018-07-13 22:01:35 +0800

    优化等待逻辑

--- xauto/web/WebUtil.py
+++ xauto/web/WebUtil.py
@@ -1,11 +1,11 @@
 from selenium import webdriver
 import time
 
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-
 
 
 class WebUtil:
@@ -22,41 +22,54 @@ class WebUtil:
     def open(self, url: str):
         self.browser.get(url)
 
-    def _find_element(self, xpath: str, second: int = 10):
-        return WebDriverWait(self.browser, second).until(EC.presence_of_element_located((By.XPATH,
-                                                                                         xpath)))
-
-    def wait(self, xpath: str, second: int = 10):
+    def wait(self, xpath: str, second: int = 15) -> bool:
         try:
-            self._find_element(xpath, second)
+            WebDriverWait(self.browser, second).until(
+                EC.presence_of_element_located((By.XPATH, xpath)))
+            return True
         except Exception:
-            pass
+            return False
 
-    def wait_multi(self, xpaths: list, second : int = 10):
+    def wait_do(self, xpath:str, func, *args):
+        self.wait(xpath)
+        func(xpath, *args)
+
+    def wait_not_exist(self, xpath: str, timeout=10) -> bool:
+        now = time.time()
+        while time.time() - now < timeout:
+            try:
+                self.browser.find_element_by_xpath(xpath)
+                time.sleep(0.1)
+            except Exception:
+                return True
+        return False
+
+    def wait_multi(self, xpaths: list, second: int = 10):
         now = time.time()
         while time.time() - now < second:
             for x in xpaths:
                 if self.browser.find_elements_by_xpath(x) is not None:
-                    return
+                    return True
                 time.sleep(0.1)
+        return False
 
     def current_url(self):
         return self.browser.current_url
 
     def click(self, xpath: str):
-        self._find_element(xpath).click()
+        self.browser.find_element_by_xpath(xpath).click()
 
     def input(self, xpath: str, text: str):
-        self._find_element(xpath).send_keys(text)
+        self.browser.find_element_by_xpath(xpath).send_keys(text)
 
     def clear(self, xpath: str):
-        self._find_element(xpath).clear()
+        self.browser.find_element_by_xpath(xpath).clear()
 
     def back(self):
         self.browser.back()
 
     def text(self, xpath: str):
-        return self._find_element(xpath).text
+        return self.browser.find_element_by_xpath(xpath).text
 
     def close_title(self, title):
         handles = self.browser.window_handles
@@ -75,28 +88,34 @@ class WebUtil:
         self.browser.quit()
 
     def submit(self, xpath: str):
-        self._find_element(xpath).submit()
+        self.browser.find_element_by_xpath(xpath).submit()
+
+    def exist(self, xpath: str, timeout: int = 3):
+        now = time.time()
+        while time.time() - now < timeout:
+            try:
+                self.browser.find_element_by_xpath(xpath)
+            except NoSuchElementException:
+                return False
+        return True
 
     def switch_to_title(self, title):
         self.browser.switch_to.window(title)
 
-    def ctrlw(self):
-        self.browser.find_element_by_xpath('//body').send_keys(Keys.CONTROL + "w")
-
-    def adblock_add_rule(self, rules = []):
+    def adblock_add_rule(self, rules=[]):
         if rules is None or len(rules) == 0:
             return
         self.open("chrome-extension://gighmmpiobklfepjocnamgkkbiglidom/options.html")
         # click custom
-        self.click('//*[@id="tabpages"]/ul/li[3]')
+        self.wait_do('//*[@id="tabpages"]/ul/li[3]', self.click)
         # click edit
-        self.click('//*[@id="btnEditAdvancedFilters"]')
+        self.wait_do('//*[@id="btnEditAdvancedFilters"]', self.click)
+        self.wait('//*[@id="txtFiltersAdvanced"]')
         # click textarea
-        textarea = self._find_element('//*[@id="txtFiltersAdvanced"]')
+        textarea = self.browser.find_element_by_xpath('//*[@id="txtFiltersAdvanced"]')
         for rule in rules:
             textarea.send_keys(Keys.ENTER)
             textarea.send_keys(rule)
         # save
         time.sleep(.2)
-        self.click('//*[@id="btnSaveAdvancedFilters"]')
-
+        self.wait_do('//*[@id="btnSaveAdvancedFilters"]', self.click)
